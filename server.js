5542089f7f51a8b82f17660527d4fe231f6c38a6
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-14 20:41:02 +0100

    Added cors and set up routes being used on server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 //Requiring in packages
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors")
+const routes = require("./routes")
 //Set up express server
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
+app.use(cors); 
+app.use(routes);
 //Assign PORT value
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 //Set up serving of static assets from build in client qwhen in production
 if(process.env.NODE_ENV === "production"){
@@ -16,7 +18,7 @@ if(process.env.NODE_ENV === "production"){
 }
 
 //Connect to Mongo DB 
-mongoose.connect("mongodb://localhost:27017/googlebooks", {
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/googlebooks", {
     useCreateIndex: true,useNewUrlParser: true, useUnifiedTopology:true
 })
 
