af1ad95299e65a06630ad20f13eb37a2792d2e9e
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-21 19:21:11 +0100

    fix: image now renders to page

--- client/src/components/BooksCards.js
+++ client/src/components/BooksCards.js
@@ -4,6 +4,7 @@ function BooksCards({title, authors, description, image, link, Button}){
     return(
         <>
         <h3>{title} by {authors}</h3>
+        <img src={image} alt={title} />
         <p>{description}</p>
         <Button />
         <button target="_blank" href={link} rel="noreferrer">Purchase</button>
