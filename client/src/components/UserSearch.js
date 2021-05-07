15dbb101f4d254dfbb0e2e747f79794797c37237
omayma ahmad <omayma.ahmad25@gmail.com>
2021-05-07 16:51:48 +0100

    styling to user search page

--- client/src/components/UserSearch.js
+++ client/src/components/UserSearch.js
@@ -1,17 +1,22 @@
 import React from "react";
+import "./UserSearch.css";
 
-function UserSearch ({q, handleSearch, handleUserInput}) {
-    return (
-        <>
-        <input type="text"
-        value={q}
-        placeholder="Search for a book..."
-        onChange={handleUserInput}
+function UserSearch({ q, handleSearch, handleUserInput }) {
+  return (
+    <>
+      <div className="wrap">
+        <input
+          type="text"
+          value={q}
+          placeholder="Search for a book..."
+          onChange={handleUserInput}
+          className="searchTerm"
         />
-        <button onClick={handleSearch}
-        type="submit">Search</button>
-        </>
-    )
+        <button onClick={handleSearch} type="submit" className="searchButton">
+          Search
+        </button>
+      </div>
+    </>
+  );
 }
-export default UserSearch; 
-
+export default UserSearch;
