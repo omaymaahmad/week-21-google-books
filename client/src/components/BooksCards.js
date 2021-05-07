246951cacf7e30764eb64abd4900846250fedc09
omayma ahmad <omayma.ahmad25@gmail.com>
2021-05-07 15:59:26 +0100

    fix: link to more information now works

--- client/src/components/BooksCards.js
+++ client/src/components/BooksCards.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-function BooksCards({title, authors, description, image, link, Button}){
-    return(
-        <>
-        <h3>{title} by {authors}</h3>
-        <img src={image} alt={title} />
-        <p>{description}</p>
-        <Button />
-        <button target="_blank" href={link} rel="noreferrer">Purchase</button>
-        </>
-    )
-
+function BooksCards({ title, authors, description, image, link, Button }) {
+  return (
+    <>
+      <h3>
+        {title} by {authors}
+      </h3>
+      <img src={image} alt={title} />
+      <p>{description}</p>
+      <Button />
+      <a href={link} rel="noopener noreferrer" target="_blank">
+        <button type="button">Click for more info</button>
+      </a>
+    </>
+  );
 }
 
-export default BooksCards
+export default BooksCards;
--- controllers/GoogleController.js
+++ controllers/GoogleController.js
@@ -1,23 +1,25 @@
 const axios = require("axios");
 
 module.exports = {
-    getGoogleBooks: function(req,res) {
-        const {query: params} = req;
+  getGoogleBooks: function (req, res) {
+    const { query: params } = req;
 
-        axios.get("https://www.googleapis.com/books/v1/volumes", {
-            params
-        }).then(results => 
-            results.data.items.filter(
-                result => 
-                result.volumeInfo.title &&
-                result.volumeInfo.infoLink &&
-                result.volumeInfo.authors &&
-                result.volumeInfo.description &&
-                result.volumeInfo.imageLinks &&
-                result.volumeInfo.imageLinks.thumbnail
-            ))
-        
-        
-        .then(results => res.json(results))
-    }
-}
+    axios
+      .get("https://www.googleapis.com/books/v1/volumes", {
+        params,
+      })
+      .then((results) =>
+        results.data.items.filter(
+          (result) =>
+            result.volumeInfo.title &&
+            result.volumeInfo.infoLink &&
+            result.volumeInfo.authors &&
+            result.volumeInfo.description &&
+            result.volumeInfo.imageLinks &&
+            result.volumeInfo.imageLinks.thumbnail
+        )
+      )
+
+      .then((results) => res.json(results));
+  },
+};
