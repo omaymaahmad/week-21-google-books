a2432094406b1b475992abd9dc2620b6a6355bfa
omayma ahmad <omayma.ahmad25@gmail.com>
2021-05-07 16:05:10 +0100

    added google react icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 class NavBar extends Component {
   render() {
     return (
       <>
         <Navbar bg="light" variant="light">
-          <Navbar.Brand href="#home">Google Books</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            {" "}
+            <FcGoogle /> Google Books
+          </Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link>
-              <Link to="/" style={{ color: "#FFF" }}>
-                Search
-              </Link>
+              <Link to="/">Search</Link>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/saved" style={{ color: "#FFF" }}>
-                Saved
-              </Link>
+              <Link to="/saved">Saved</Link>
             </Nav.Link>
           </Nav>
         </Navbar>
